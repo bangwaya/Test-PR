cfb0abd6278e6ed55ba10abe02462a67dfa1a1de
Pangwawaa <52168329+Pangwawaa@users.noreply.github.com>
2020-11-02 17:27:25 +0800

    Blala (#3)

---
+++ hal_sd_ksyscall.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (C) 2015-2019 Alibaba Group Holding Limited
+ */
+
+#ifndef HAL_SD_KSYSCALL_H
+#define HAL_SD_KSYSCALL_H
+
+int32_t sys_hal_sd_init_stub(void *arg);
+
+int32_t sys_hal_sd_blks_read_stub(void *arg);
+
+int32_t sys_hal_sd_finalize_stub(void *arg);
+
+#endif /* HAL_SD_KSYSCALL_H */
+
